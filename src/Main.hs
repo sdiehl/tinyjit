05ba42f403b25d1da28efc45ed0c13aa012d96be
Stephen Diehl <stephen.m.diehl@gmail.com>
2016-01-05 19:07:54 -0500

    clarify main module

--- src/Main.hs
+++ src/Main.hs
@@ -11,9 +11,7 @@ import Foreign.C.Types
 import JIT
 import Assembler
 
--- Return Value: rax, rdx
--- Parameter Registers: rdi, rsi, rdx, rcx, r8, r9
-
+-- Example 1
 arith :: X86 ()
 arith = do
   mov rax (I 18)
@@ -26,6 +24,17 @@ arith = do
   mul rax
   ret
 
+-- Example 2
+factorial :: Int64 -> X86 ()
+factorial n = do
+  mov rcx (I n)
+  mov rax (I 1)
+  l1 <- label
+  mul rcx
+  loop l1
+  ret
+
+-- Example 3
 printf :: Word32 -> Word32 -> X86 ()
 printf fnptr msg = do
   push rbp
@@ -34,15 +43,6 @@ printf fnptr msg = do
   call (A fnptr)
   pop rbp
   mov rax (I 0)
-  ret
-
-factorial :: Int64 -> X86 ()
-factorial n = do
-  mov rcx (I n)
-  mov rax (I 1)
-  l1 <- label
-  mul rcx
-  loop l1
   ret
 
 dump :: [Word8] -> IO ()
@@ -56,9 +56,9 @@ main = do
   msg <- asciz "Hello Haskell"
   mem <- allocateMemory jitsize
 
-  let jitm = assemble mem (printf fn msg)
   {-let jitm = assemble mem arith-}
-  {-let jitm = assemble mem (factorial 5)-}
+  let jitm = assemble mem (factorial 5)
+  {-let jitm = assemble mem (printf fn msg)-}
 
   case jitm of
     Left err -> putStrLn err
